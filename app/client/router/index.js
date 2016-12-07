190603006b94f92e864dc0e465d9e60027979c28
luckyadam <weitaozsh@gmail.com>
2016-12-07 14:57:49 +0800

    update:改变路由切换后页面标题

--- app/client/router/index.js
+++ app/client/router/index.js
@@ -6,20 +6,36 @@ import Project from '../views/Project';
 
 Vue.use(Router);
 
-export default new Router({
+const router =  new Router({
   mode: 'hash',
   routes: [
     {
       path: '/',
-      component: Home
+      component: Home,
+      meta: {
+        title: '统一上线平台'
+      }
     },
     {
       path: '/projectList',
-      component: Projects
+      component: Projects,
+      meta: {
+        title: '统一上线平台 - 项目列表'
+      }
     },
     {
       path: '/projectList/:id',
-      component: Project
+      component: Project,
+      meta: {
+        title: '统一上线平台 - 项目详情'
+      }
     }
   ]
 });
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title || '统一上线平台';
+  next();
+});
+
+export default router;
